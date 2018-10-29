6a1bf62038afd0c19eb8b1bd544372576ec92c8e
JACOB-WORKSTATI\douz0 <jacobadozier@gmail.com>
2018-10-28 22:41:32 -0500

    Added /signup to render template index.html.

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # Displays runtime errors in the browser, too
 
-@app.route("/signup" methods=['POST'])
+@app.route("/signup", methods=['POST'])
 def user_signup():
-    
+    return render_template('index.html')
 
 @app.route("/")
 def index():
-    return render_template('base.html', )
+    return render_template('index.html', )
 
 app.run()
