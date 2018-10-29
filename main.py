a98898775746e6cf016ddc8316da21b0b57a85f8
JACOB-WORKSTATI\douz0 <jacobadozier@gmail.com>
2018-10-28 21:10:34 -0500

    Added app.route(/) and def index.

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from flask import Flask, request, redirect, render_template
 import cgi
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True      # Displays runtime errors in the browser, too
+
+
+
+@app.route("/")
+def index():
+    return render_template('base.html', )
+
+app.run()
