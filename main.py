88303299ea3d97a1dfbb1f5f314fa4ee2e44c45f
JACOB-WORKSTATI\douz0 <jacobadozier@gmail.com>
2018-10-28 23:26:26 -0500

    Modified user_signup() to return username and email to form.

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # Displays runtime errors in the browser, too
 
+
+
 @app.route("/signup", methods=['POST'])
 def user_signup():
-    return render_template('index.html')
+    username_input = request.form['username']
+    email_input = request.form['email']
+
+    # Return user input in username and email fields
+    return render_template('index.html', username=username_input, email=email_input)
 
 @app.route("/")
 def index():
